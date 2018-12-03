209385d954d97d78d2c3576873d6aab3ecc4b34a
Christopher Pace <Christopher_pace@ymail.com>
2018-12-03 09:46:04 -0600

    check in

---
+++ key.js
@@ -0,0 +1,13 @@
+array = [[2, 3], [4, 3, 2]];
+
+
+for (i = 0; i < array.length; i++) {
+    var array2 = array + "";
+    for (x = 0; x < a.length; x++) {
+
+        console.log(array2)
+    }
+
+
+}
+
